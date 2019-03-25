e414ac59367a2011e84293e8df87654b8be599be
MR0KING <ardiansyah282003@gmail.com>
2019-03-25 19:29:01 +0700

    ToolsInstallerv1

---
+++ ToolsInstallerv1/ToolsInstallerv1.sh
@@ -0,0 +1,94 @@
+#!/system/bin/sh
+#!/bin/bash
+
+
+clear
+sleep 3
+echo '\033[0;34m###########################################################'
+toilet -f slant --gay "  WELCOME"
+echo '\033[0;34m###########################################################'
+sleep 2
+echo '\033[0;33m                        WHO ARE YOU'
+sleep 1
+read -p 'MR.0KING~# ' pilih
+case $pilih in
+'Who Am I')
+echo ''
+sleep 3
+clear
+echo '\033[0;32m                       [✓]Welcome[✓]'
+sleep 3
+clear
+
+git clone https://github.com/4L13199/LITESPAM
+git clone https://github.com/Amriez/gcospam
+git clone https://github.com/Gameye98/santet-online
+git clone https://github.com/Xeit666h05t/CrewBot
+git clone https://github.com/4L13199/LITEDDOS
+git clone https://github.com/maldevel/IPGeolocation
+git clone https://github.com/avramit/instahack
+git clone https://github.com/cyweb/hammer
+git clone https://github.com/CiKu370/OSIF.git
+
+clear
+echo '\033[0;31mLoading'
+sleep 1
+clear
+echo '\033[0;31mLoading.'
+sleep 1
+clear
+echo '\033[0;31mLoading..'
+sleep 1
+clear
+echo '\033[0;31mLoading...'
+sleep 1
+clear
+echo '\033[0;31mLoading....'
+sleep 2
+clear
+echo '\033[0;32m                       [✓]Welcome[✓]'
+echo '                  [!]Jika Ada Masalah[!]'
+echo '                [!] Hubungi Sang Master[!]'
+echo '               #############################'
+echo '               #          MR.0KING         #'
+echo '               #          MR-CYBER         #'
+echo '               #        089638787729       #'
+echo '               #############################'
+echo '        ~•TIDAK ADA YANG MUSTAHIL UNTUK DILAKUKAN•~'
+sleep 2
+echo '###########################################################'
+ls
+echo '\033[0;32m###########################################################'
+sleep 2
+;;
+*)
+clear
+echo '\033[0;31mLoading'
+sleep 1
+clear
+echo '\033[0;31mLoading.'
+sleep 1
+clear
+echo '\033[0;31mLoading..'
+sleep 1
+clear
+echo '\033[0;31mLoading...'
+sleep 1
+clear
+echo '\033[0;31mLoading....'
+echo '\033[0;31m'
+sleep 3
+clear
+echo '                        [!]Failed[!]'
+sleep 2
+echo '                   [!]Jika Ada Masalah[!]'
+echo '                 [!] Hubungi Sang Master[!]'
+echo '                #############################'
+echo '                #          MR.0KING         #'
+echo '                #          MR-CYBER         #'
+echo '                #        089638787729       #'
+echo '                #############################'
+echo '        ~•TIDAK ADA YANG MUSTAHIL UNTUK DILAKUKAN•~'
+sleep 2
+;;
+esac
